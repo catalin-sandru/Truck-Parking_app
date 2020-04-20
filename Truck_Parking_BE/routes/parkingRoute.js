7e83b86fdf8c6a7cf1e28419d7e319cf4653bf0f
freebiker2000 <catalin.sandru88@gmail.com>
2020-04-20 18:48:08 +0100

    started to add backend validation

--- Truck_Parking_BE/routes/parkingRoute.js
+++ Truck_Parking_BE/routes/parkingRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, check } = require('express-validator');
 
 const ParkingController = require('../controller/parkingController');
 
@@ -7,6 +8,11 @@ const router = express.Router();
 router.get('/', ParkingController.getAllRegions);
 
 router.post('/add-region', ParkingController.addRegion);
-router.post('/region/:id', ParkingController.addParkingSpot);
+router.post('/region/:id', [
+  body('title')
+    .trim()
+    .isString()
+    .isLength({min: 3, max:150})
+], ParkingController.addParkingSpot);
 
 module.exports = router;
