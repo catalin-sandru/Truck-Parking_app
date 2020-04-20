72151e256a43f9eed483a10252cf0bdd06aac04b
freebiker2000 <catalin.sandru88@gmail.com>
2020-04-20 20:38:57 +0100

    added coordinates validation with regex expression

--- Truck_Parking_BE/routes/parkingRoute.js
+++ Truck_Parking_BE/routes/parkingRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { body, check } = require('express-validator');
 
+const regEx = require('../util/regex');
 const ParkingController = require('../controller/parkingController');
 
 const router = express.Router();
@@ -12,7 +13,19 @@ router.post('/region/:id', [
   body('title')
     .trim()
     .isString()
-    .isLength({min: 3, max:150})
+    .isLength({min: 3, max:150}),
+  body('coordonates')
+    .trim()
+    .isString()
+    .isLength(25).withMessage('Coordonates must have 24 charachters')
+    .matches(regEx),
+  body('facilities')
+    .not()
+    .isEmpty()
+    .withMessage('Please select at leat one facility at this spot'),
+  body('extra_info')
+    .trim()
+    .isString()
 ], ParkingController.addParkingSpot);
 
 module.exports = router;
