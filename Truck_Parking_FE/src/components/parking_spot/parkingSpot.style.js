10e87e8041bab00692567038ad71ecf70aab4393
freebiker2000 <catalin.sandru88@gmail.com>
2020-05-02 14:20:01 +0100

    added some style

--- Truck_Parking_FE/src/components/parking_spot/parkingSpot.style.js
+++ Truck_Parking_FE/src/components/parking_spot/parkingSpot.style.js
@@ -4,5 +4,27 @@ export const ParkingSpotStyle = styled.div`
   .parking__spot {
     border: 1px solid #000;
     margin: 0.2rem 0.4rem;
+    padding: 0.5rem;
+
+    .parking__spot-title {
+      font-size: 1.1rem;
+      border-bottom: 1px solid #ccc;
+    }
+
+    .parking__spot-coordinates {
+      img {
+        width: 13px;
+      }
+    }
+
+    .parking__spot-icons {
+      margin: 0.3rem 0;
+      img {
+        width: 40px;
+      }
+    }
+    .parking__spot-info {
+      border-top: 1px solid #ccc;
+    }
   }
 `
---
+++ Truck_Parking_FE/src/pages/region/RegionComponent/Region.stye.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const RegionStyle = styled.div`
+  header{
+    button{
+      margin: 0.4rem;
+      font-size: 1rem;
+      padding: 0.3rem;
+      text-align: center;
+    }
+  }
+`
